2b43cadef4069304a34b2f04b52ec54297c0e189
Carlos Gonzaga <cgonzagaskill2022@outlook.com>
2026-08-11 12:51:30 -0300

    feat: add first part the project

---
+++ src/br/local/screematch/versao_00/Main.java
@@ -0,0 +1,15 @@
+package br.local.screematch.versao_00;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Esse á o Screen Match");
+        System.out.println("FIlme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano Lançamento: " + anoDeLancamento);
+
+        boolean incluidoNoPlano = false;
+        double notaDoFilme;
+
+    }
+}
