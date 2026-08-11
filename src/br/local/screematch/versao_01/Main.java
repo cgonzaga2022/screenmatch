92601e4ef94f23c64d59f573bbedf2cfaea953f5
Carlos Gonzaga <cgonzagaskill2022@outlook.com>
2026-08-11 14:32:57 -0300

    feat: add new itens

---
+++ src/br/local/screematch/versao_01/Main.java
@@ -0,0 +1,25 @@
+package br.local.screematch.versao_01;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Esse á o Screen Match");
+        System.out.println("FIlme: Top Gun: Maverick");
+
+        int andDeLancamento = 2022;
+        System.out.println("Ano de Lancamento: " + andDeLancamento);
+        boolean incluidoNoPlano = false;
+        double notaDoFilme = 8.1;
+
+        double media = 9.2 + 6.3 + 3.8 / 3;
+        System.out.printf("Media do filme: %.2f",  media);
+        System.out.println(String.format("\nA méidia é  %.2f", media));
+
+        String sinopse = """ 
+        Filme Top Gun
+        Filme de aventura com galã dos anos 80";
+        Media de nota do filme %.2f
+        """.formatted(media);
+        System.out.println("Sinopse: " + sinopse);
+    }
+
+}
